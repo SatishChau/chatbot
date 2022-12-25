638af3a3fa8bcb69d8573249cc0576a2ab8fd4ee
SatishChau <72039377+SatishChau@users.noreply.github.com>
2022-12-25 11:14:45 +0530

    For Hosting purpose

--- main.py
+++ main.py
@@ -62,12 +62,12 @@ def main() -> None:
     dispatcher.add_handler(MessageHandler(Filters.text, reply))
 
     updater.start_polling()
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=int(PORT),
-    #                       url_path='1758805627:AAHWjp0lHW5yYvEbhy19f3DBwPB9mS2Z1f0')
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path='1758805627:AAHWjp0lHW5yYvEbhy19f3DBwPB9mS2Z1f0')
 
-    # updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + '1758805627:AAHWjp0lHW5yYvEbhy19f3DBwPB9mS2Z1f0')
-    updater.idle()
+    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + '1758805627:AAHWjp0lHW5yYvEbhy19f3DBwPB9mS2Z1f0')
+#     updater.idle()
 
 if __name__ == '__main__':
     main()
